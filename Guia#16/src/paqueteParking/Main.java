86839a07ad7350a8b576eade755a5baff554ae1e
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-23 15:25:03 -0500

    Variables globales

---
+++ Guia#16/src/paqueteParking/Main.java
@@ -0,0 +1,23 @@
+
+package paqueteParking;
+
+import guia.views.Window;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+
+public static void main(String[]args){
+    
+Window ventana = new Window();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+     
+    
+}
+     
+    
+}
